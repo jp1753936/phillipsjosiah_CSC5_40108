ccff16c0525849e4a6fef96b1cf3fdcbc9e17855
Josiah I Phillips <phillipsjosiah3@gmail.com>
2017-01-12 14:38:44 -0800

    Add files via upload

---
+++ homwk/Assignment _1/Gaddis_7thEd_Chap2_Prob1_Sum/main.cpp
@@ -0,0 +1,29 @@
+/* 
+  File:   main.cpp
+  Author: Josiah Phillips
+  Created on January 10, 2017, 9:49 PM
+  Purpose:  Sum of Two Numbers
+ */
+
+#include <iostream>
+ using namespace std;
+
+int main()
+
+{
+
+int numberone = 62;
+
+int numbertwo = 99;
+
+int total = numberone + numbertwo;
+
+//programology
+
+cout << "The sum of " << numberone << " + " << numbertwo << " = " << total << endl;
+
+cout << endl;
+
+return 0;
+
+}
