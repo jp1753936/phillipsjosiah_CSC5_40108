910c2d7b0cba52eef1a4e82801a0ff49fa012629
Josiah I Phillips <phillipsjosiah3@gmail.com>
2017-01-12 14:39:51 -0800

    Add files via upload

---
+++ homwk/Assignment _1/Gaddis_7thED_Chap2_Prob11_Distance per Tank of Gas/main.cpp
@@ -0,0 +1,34 @@
+/* 
+  File:   main.cpp
+  Author: Josiah Phillips
+  Created on January 10, 2017, 11:20 PM
+  Purpose:  Distance per Tank of Gas
+ */
+
+#include <iostream>
+using namespace std;
+	 
+int main()
+
+{
+
+int distance;
+
+cout << "Town: ";
+
+distance = 20 * 21.5;
+
+cout << " "
+
+<< distance << endl;
+
+cout << "Highway: ";
+
+distance = 20 * 26.8;
+
+cout << " "
+
+<< distance << endl;
+
+	}
+
